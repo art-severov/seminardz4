daf8f510523b875cc295c0f82e38196984230558
Artem <art-severov@yandex.ru>
2023-01-26 18:59:03 +0300

    подготовленно

---
+++ zadacha27/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+// Console.WriteLine("Введите число:");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// int Summ(int number)
+
+// {
+// int count = 
+// }
+
+
+// int number = int.Parse(Console.ReadLine());
+// int summ = 0;
+//   while (number > 0)
+//   {
+
+// summ = summ + number % 10;
+// number = number /10 ;
+
+//    }  
+// Console.WriteLine(summ);
+
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int summ = Summ(number);
+Console.WriteLine(summ);
+
+
+int Summ (int number)
+{
+    int counter = number;
+    int summ = 0;
+    int result = 0;
+    for (int i = 0; i < counter; i++)
+    {
+     summ = number - number % 10;
+        result = result + (number - summ);
+        number = number / 10;
+    }
+    return result;
+} 
+
---
+++ zadacha29/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+
+
+int[] GenerateArray(int size,int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random(); 
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} " );
+      
+    }
+}
+
+int [] array = GenerateArray(8, 0, 99);
+PrintArray(array);
+Console.Write($"->[ "); 
+PrintArray(array);
+Console.Write($"]"); 
+
+
